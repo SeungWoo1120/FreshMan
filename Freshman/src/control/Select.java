edb90493685e94d325fc95057ef09a8d35fd9fae
rnrgh <rnrgh@DESKTOP-OVR3UFT>
2020-07-22 13:09:17 +0900

    계산기 만든거 next와 nextLine(nextInt 뒤에 쓰면 엔터를 먹음)

--- Freshman/src/control/Select.java
+++ Freshman/src/control/Select.java
@@ -1,21 +1,30 @@
 package control;
 
+import java.util.Scanner;
+
 public class Select {
 	public static void main(String[] args) {
-		int a = 2;
 		
-		switch(a) {
-		case 0 :
-			System.out.println(0);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("첫 번째 숫자를 입력하시오");
+		int a = sc.nextInt();
+		System.out.println("부호를 입력하시오");
+		String buho = sc.next();
+		System.out.println("두 번째 숫자를 입력하시오");
+		int b = sc.nextInt();
+		
+		switch(buho) {
+		case "+" :
+			System.out.println(a + " + " + b + " = " + (a + b));
 			break;
-		case 1 :
-			System.out.println(1);
+		case "-" :
+			System.out.println(a + " - " + b + " = " + (a - b));
 			break;
-		case 2 :
-			System.out.println(2);
+		case "*" :
+			System.out.println(a + " * " + b + " = " + (a * b));
 			break;
-		case 3 :
-			System.out.println(3);
+		case "/" :
+			System.out.println(a + " / " + b + " = " + (double)a / b);
 			break;
 		}
 	}
