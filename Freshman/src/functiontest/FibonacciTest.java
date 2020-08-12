ff191d257f46d04775c49881881f73824d5fa3ca
SeungWoo1120 <rnrghs12@naver.com>
2020-08-12 16:34:02 +0900

    함수를 사용한 피보나치수열

--- Freshman/src/functiontest/FibonacciTest.java
+++ Freshman/src/functiontest/FibonacciTest.java
@@ -4,26 +4,30 @@ import java.util.Scanner;
 
 public class FibonacciTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Scanner sc = new Scanner(System.in);
-		System.out.println("원하는 수열 개수를 입력하시오.");
-		int num = sc.nextInt();
-
-		int array[] = new int[num];
-		
-		for (int i = 0; i < num; i++) {
+	
+	static int[] fibo(int number) {
+		int array[] = new int[number];
+		for (int i = 0; i < number; i++) {
 			if(i == 0 || i == 1) {
 				array[i] = 1;
 			} else {
 				array[i] = array[i - 2] + array[i - 1];				
 			}
 		}
-		
+		return array;
+	}
+	static void print(int[] array) {
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
-
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("원하는 수열 개수를 입력하시오.");
+		int num = sc.nextInt();
+		print(fibo(num));
+	
 	}
 }
