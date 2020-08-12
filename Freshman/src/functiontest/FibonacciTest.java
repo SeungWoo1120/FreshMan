238a9a0be2c9d6493da95726489e646d6dd048f2
SeungWoo1120 <rnrghs12@naver.com>
2020-08-12 16:28:05 +0900

    피보나치수열(형과 수정 후)

--- Freshman/src/functiontest/FibonacciTest.java
+++ Freshman/src/functiontest/FibonacciTest.java
@@ -6,29 +6,24 @@ public class FibonacciTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("원하는 수열 개수를 입력하시오.");
 		int num = sc.nextInt();
-		
-		int array[] = new int[num];
 
-		array[0] = 1;
-		//array[1] = 1; ★
+		int array[] = new int[num];
 		
-		if(num == 1){
-			System.out.println(array[0]);
-		} else if(num == 2) {
-			System.out.println(array[0] + " " + array[1]);
-		} else {
-			for (int i = 2; i < num; i++) {
-				array[i] = array[i-2] + array[i-1];
-			}
-			System.out.print(array[0] + " " + array[1] + " ");
-			for (int i = 2; i < num; i++) {
-				System.out.print(array[i] + " ");
+		for (int i = 0; i < num; i++) {
+			if(i == 0 || i == 1) {
+				array[i] = 1;
+			} else {
+				array[i] = array[i - 2] + array[i - 1];				
 			}
 		}
-	
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+
 	}
 }
