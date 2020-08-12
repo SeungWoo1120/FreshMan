64241029ded6b1628a206d244e975c612a5819ec
SeungWoo1120 <rnrghs12@naver.com>
2020-08-12 17:58:48 +0900

    클래스 부분 반절 공부

---
+++ Freshman/src/classtest/Main.java
@@ -0,0 +1,15 @@
+package classtest;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node nd = new Node(10, 10);
+		Node nd1 = new Node(5, 10);
+		
+		//(nd.center(nd1).getX() + ", " + nd.center(nd1).getY());
+		nd.print();
+		
+	}
+
+}
--- Freshman/src/functiontest/FibonacciTest.java
+++ Freshman/src/functiontest/FibonacciTest.java
@@ -16,11 +16,13 @@ public class FibonacciTest {
 		}
 		return array;
 	}
+	
 	static void print(int[] array) {
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
